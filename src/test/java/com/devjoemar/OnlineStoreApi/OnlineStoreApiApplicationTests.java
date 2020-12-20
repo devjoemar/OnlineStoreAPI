a84aa800877d4f33f4626bd3301447ef2bb627b7
Joemar Polinar <devjoemar2016@gmail.com>
2020-12-20 17:21:58 +0800

    Initial Commit

---
+++ src/test/java/com/devjoemar/OnlineStoreApi/OnlineStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoemar.OnlineStoreApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
